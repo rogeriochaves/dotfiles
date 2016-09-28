9295495ed71eb34dd9162bd7373c7a8d106aae80
Rogerio Chaves <rogeriocfj+github@gmail.com>
2016-09-28 14:41:35 -0300

    Add tabbar manual step to setup

--- setup.sh
+++ setup.sh
@@ -29,6 +29,7 @@ brew tap d12frosted/emacs-plus
 brew install emacs-plus --with-cocoa --with-gnutls --with-librsvg --with-imagemagick --with-spacemacs-icon
 brew linkapps
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+git clone https://github.com/evacchi/tabbar-layer ~/.emacs.d/private/tabbar
 
 # speed up keystroke
 defaults write -g KeyRepeat -int 0.5
